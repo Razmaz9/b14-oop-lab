41d956432624b45e48254b18e94f61f8e7ac9c44
Aron Larsson <arlars03@gmail.com>
2023-11-28 13:13:49 +0100

    Implement new "Last in first out" storage interface

--- lab1-master/src/CarTransport.java
+++ lab1-master/src/CarTransport.java
@@ -4,21 +4,23 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarTransport extends Car implements StorageThings{
+public class CarTransport extends Car implements LIFOStorage<Car> {
 
     private List<Car> storage = new ArrayList<>();
     private boolean platformIsOpen = true;
+    private int maxStorage;
 
     public CarTransport() {
         super(2, 400, Color.CYAN, "Example Car Transport");
+        setMaxStorage(8);
     }
 
 
-    public void openPlatform(){
+    public void openPlatform() {
         platformIsOpen = true;
     }
 
-    public void closePlatform(){
+    public void closePlatform() {
         platformIsOpen = false;
     }
 
@@ -36,16 +38,26 @@ public class CarTransport extends Car implements StorageThings{
     }
 
     @Override
-    public void removeVehicle(){
-        int lastIndex = getStorage().size() -1 ;
+    public int getMaxStorage() {
+        return maxStorage;
+    }
+
+    @Override
+    public void setMaxStorage(int num) {
+        maxStorage = num;
+    }
+
+    @Override
+    public void removeLastItem() {
+        int lastIndex = getStorage().size() - 1;
         Car vehicle = getStorage().get(lastIndex);
-        if(isPlatformOpen())
+        if (isPlatformOpen())
             getStorage().remove(lastIndex);
         vehicle.setXCoordinate(getXCoordinate() - 1);
         vehicle.setYCoordinate(getYCoordinate() - 1);
     }
 
-    private boolean checkIfLoadable(Car vehicle){
+    private boolean checkIfLoadable(Car vehicle) {
         double xDifferential = getXCoordinate() - vehicle.getXCoordinate();
         double yDifferential = getYCoordinate() - vehicle.getYCoordinate();
         return Math.abs(xDifferential) < 1 && Math.abs(yDifferential) < 1;  // If car is 1 coordinate unit away from car transport, return true, else false.
@@ -53,11 +65,11 @@ public class CarTransport extends Car implements StorageThings{
 
     @Override
     public void move() {
-        if(!isPlatformOpen()){
+        if (!isPlatformOpen()) {
             super.move();
             for (Car vehicle : getStorage()) {
-              vehicle.setXCoordinate(getXCoordinate());
-              vehicle.setYCoordinate(getYCoordinate());
+                vehicle.setXCoordinate(getXCoordinate());
+                vehicle.setYCoordinate(getYCoordinate());
             }
         }
     }
