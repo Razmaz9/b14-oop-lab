a54954c71235d1e85c3d8cc15f4872e12c0d4fb1
Aron Larsson <arlars03@gmail.com>
2023-11-28 14:28:32 +0100

    Change order of modifiers

--- lab1-master/src/Volvo240.java
+++ lab1-master/src/Volvo240.java
@@ -2,7 +2,7 @@ package src;
 import java.awt.*;
 
 public class Volvo240 extends Car{
-    private final static double trimFactor = 1.25;
+    private static final double trimFactor = 1.25;
     public Volvo240(){
         super(4, 100, Color.BLACK, "Volvo240");
     }
