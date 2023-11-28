439c85b470b2249f1cb02e75d05cf021043137f7
Aron Larsson <arlars03@gmail.com>
2023-11-28 11:25:06 +0100

    Make fields private and add relevant getters and setters

--- lab1-master/src/Car.java
+++ lab1-master/src/Car.java
@@ -1,50 +1,61 @@
 package src;
 
 import java.awt.*;
+
 public abstract class Car implements Movable {
-        private final String[] directions = {"N", "E", "S", "W"};
-        public int nrDoors; // Number of doors on the car
-        public double enginePower; // Engine power of the car
-        public double speedFactor;
-        private double currentSpeed; // The current speed of the car must be public
-        public Color color; // Color of the car
-        public String modelName; // The car model name
-        public int direction; // The direction that the car is facing
-        public double xCoordinate; // The x coordinate of the car
-        public double yCoordinate; // The Y coordinate of the car
-        public int getNrDoors(){
-            return nrDoors;
-        }
-        public double getEnginePower(){
-            return enginePower;
-        }
-        public double getCurrentSpeed(){
-            return currentSpeed;
-        }
-        public Color getColor(){
-            return color;
-        }
-        public String getDirection() {
-            return directions[direction];
-        }
-        public double getXCoordinate(){
-            return xCoordinate;
-        }
-        public double getYCoordinate(){
-            return yCoordinate;
-        }
-        public String getModelName(){
-            return modelName;
-        }
-        public void setColor(Color clr){
-            color = clr;
-        }
-        public void startEngine(){
-            currentSpeed = 0.1;
-        }
-        public void stopEngine(){
-            currentSpeed = 0;
-        }
+    private final String[] directions = {"N", "E", "S", "W"};
+    private int direction; // The direction that the car is facing
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double speedFactor;
+    private double currentSpeed; // The current speed of the car must be public
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+    private double xCoordinate; // The x coordinate of the car
+    private double yCoordinate; // The Y coordinate of the car
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color clr) {
+        color = clr;
+    }
+
+    public String getDirection() {
+        return directions[direction];
+    }
+
+    public double getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public double getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void startEngine() {
+        setCurrentSpeed(0.1);
+    }
+
+    public void stopEngine() {
+        setCurrentSpeed(0);
+    }
 
         @Override
         public void move() {
@@ -91,4 +102,23 @@ public abstract class Car implements Movable {
             }
         }
 
+    public double getSpeedFactor() {
+        return speedFactor;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void setCurrentSpeed(double currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setXCoordinate(double xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public void setYCoordinate(double yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 }
