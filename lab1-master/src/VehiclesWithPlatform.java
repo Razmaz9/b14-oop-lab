a311726c672b394275b89df211732af4f02e5378
Aron Larsson <arlars03@gmail.com>
2023-11-28 11:47:50 +0100

    Make fields private and create getters and setter

--- lab1-master/src/VehiclesWithPlatform.java
+++ lab1-master/src/VehiclesWithPlatform.java
@@ -4,9 +4,9 @@ import java.awt.*;
 
 public abstract class VehiclesWithPlatform extends Car implements Movable {
 
-    public int platformAngle;
-    public int maxAngle;
-    public int minAngle;
+    private int platformAngle;
+    private int maxAngle;
+    private int minAngle;
 
     public VehiclesWithPlatform(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
@@ -19,12 +19,18 @@ public abstract class VehiclesWithPlatform extends Car implements Movable {
 
     public void setPlatformAngle(int angle) {
         if (getCurrentSpeed() == 0) {
-            if (angle <= minAngle) {
-                platformAngle = minAngle;
+            if (angle <= getMinAngle()) {
+                platformAngle = getMinAngle();
             } else
-                platformAngle = Math.min(angle, maxAngle);
+                platformAngle = Math.min(angle, getMaxAngle());
         }
     }
 
-}
+    public int getMaxAngle() {
+        return maxAngle;
+    }
 
+    public int getMinAngle() {
+        return minAngle;
+    }
+}
