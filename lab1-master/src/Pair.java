6fc6cec97433d4e6c39b60b3dc4923b57b763cee
Aron Larsson <arlars03@gmail.com>
2023-12-06 14:10:05 +0100

    Create generic type for holding a pair

---
+++ lab1-master/src/Pair.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.awt.*;
+
+public class Pair<T, T1> {
+
+    T first;
+    T1 second;
+    public Pair(T first, T1 second) {
+        this.first = first;
+        this.second = second;
+    }
+}
