b1230f1c4eb83d75ad202f7188bd238ef6d95fe9
Aron Larsson <arlars03@gmail.com>
2023-12-06 10:37:02 +0100

    Change bound of parameter type

--- lab1-master/src/RAStorage.java
+++ lab1-master/src/RAStorage.java
@@ -1,6 +1,6 @@
 package src;
 
-public interface RAStorage<T extends Car> extends Storage<T> {
+public interface RAStorage<T extends Storable> extends Storage<T> {
     // "Random Access Storage", any car can be removed from the storage
     void removeItem(T item);
 }
