84727119019e76e2158747eb233204d4dca6edb0
Aron Larsson <arlars03@gmail.com>
2023-12-06 11:00:37 +0100

    Fix: make StorableCar implement Storable instead of Car

--- lab1-master/src/StorableCar.java
+++ lab1-master/src/StorableCar.java
@@ -2,7 +2,7 @@ package src;
 
 import java.awt.*;
 
-public abstract class StorableCar extends Car {
+public abstract class StorableCar extends Car implements Storable {
     protected StorableCar(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
     }
